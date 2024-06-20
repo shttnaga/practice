bee92a6f10bbf10a7dff622f6780837604fb2604
DESKTOP-LKAFOMS\株式会社SHT <k_shioiri@shtech.co.jp>
2024-06-20 16:37:04 +0900

    HTMLの追加、serviceの編集

--- ShoppingWeb/src/main/java/sample/Service/AddNewItemService.java
+++ ShoppingWeb/src/main/java/sample/Service/AddNewItemService.java
@@ -3,10 +3,22 @@ package sample.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import sample.Repository.AddNewItemRepository;
+import sample.Entity.ShoppingItem;
+import sample.Repository.ShopRepository;
 
 @Service
 public class AddNewItemService {
 	@Autowired
-	AddNewItemRepository addNewItemRepository;
+	ShopRepository shopRepository;
+	
+    public ShoppingItem AddrNewItem(String product_name, Integer price, Integer quantity) {
+        // ShoppingItemエンティティを作成して情報をセット
+        ShoppingItem newItem = new ShoppingItem();
+        newItem.setProduct_name(product_name);
+        newItem.setPrice(price);
+        newItem.setQuantity(quantity);
+
+        // データベースに保存
+        return shopRepository.save(newItem);
+    }
 }
