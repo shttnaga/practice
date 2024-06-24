8d9f865a51529dd13cb6c964cde9bcd09d79f88b
taka <t_nagai@shtech.co.jp>
2024-06-24 12:00:44 +0900

    個人的な修正

--- ShoppingWeb/src/main/java/sample/Repository/ShopRepository.java
+++ ShoppingWeb/src/main/java/sample/Repository/ShopRepository.java
@@ -1,11 +1,17 @@
 package sample.Repository;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import sample.Entity.ShoppingItem;
 @Repository
 public interface ShopRepository extends CrudRepository< ShoppingItem, Integer>{
-	
+	@Modifying
+	@Query(value="UPDATE product SET quantity= :quantity WHERE product_id= :product_id")
+	void changeQuantity(@Param("quantity") Integer quantity,@Param("product_id")Integer product_id);
+    boolean existsByProductName(String productName);
 }
 
--- ShoppingWeb/src/main/java/sample/Service/ShopService.java
+++ ShoppingWeb/src/main/java/sample/Service/ShopService.java
@@ -17,4 +17,18 @@ public class ShopService {
     public List<ShoppingItem> findAll() {
         return (List<ShoppingItem>) shopRepository.findAll(); // corrected the syntax
     }
+    public ShoppingItem AddNewItem(String product_name, Integer price, Integer quantity) {
+  	  // 既存の商品名チェック
+      if (shopRepository.existsByProductName(product_name)) {
+          throw new RuntimeException("同じ商品名の商品が既に存在します。");
+      }
+      // ShoppingItemエンティティを作成して情報をセット
+      ShoppingItem newItem = new ShoppingItem();
+      newItem.setProduct_name(product_name);
+      newItem.setPrice(price);
+      newItem.setQuantity(quantity);
+      // データベースに保存
+      return shopRepository.save(newItem);
+
+  }
 }
